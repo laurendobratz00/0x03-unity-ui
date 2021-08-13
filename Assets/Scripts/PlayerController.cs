e4dad7cae7e738014b9b383d32bd4e20cc66972a
laurendobratz00 <61027706+laurendobratz00@users.noreply.github.com>
2021-08-12 19:17:04 -0500

    add everything AGAIN

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,58 +1,94 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 1000f;
-    public Rigidbody r;
+    public Rigidbody rb;
     private int score = 0;
     public int health = 5;
+    public Text scoreText;
+    public Text healthText;
+    public Text WinLoseText;
+    public GameObject youWinUI;
+    public Image WinLoseBG;
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey("a"))
-        {
-            r.AddForce(-speed * Time.deltaTime, 0, 0);
-        }
         if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey("d"))
         {
-            r.AddForce(speed * Time.deltaTime, 0, 0);
+            rb.AddForce(speed * Time.deltaTime, 0, 0);
         }
+
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey("a"))
+        {
+            rb.AddForce(-speed * Time.deltaTime, 0, 0);
+        }
+
         if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey("w"))
         {
-            r.AddForce(0, 0, speed * Time.deltaTime);
+            rb.AddForce(0, 0, speed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey("s"))
         {
-            r.AddForce(0, 0, -speed * Time.deltaTime);
+            rb.AddForce(0, 0, -speed * Time.deltaTime);
         }
     }
-    void OnTriggerEnter(Collider other)
+void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Pickup")
         {
-            score++;
-            Debug.Log("Score: " + score);
+            score += 1;
+            SetScoreText();
             Destroy(other.gameObject);
         }
+
         if (other.gameObject.tag == "Trap")
         {
-            health--;
-            Debug.Log("Health: " + health);
+            health -= 1;
+            SetHealthText();
         }
+
         if (other.gameObject.tag == "Goal")
         {
-            Debug.Log("You win!");
+            youWinUI.SetActive(true);
+            WinLoseText.text = "You Win!";
+            WinLoseBG.color = Color.green;
+   
+            WinLoseText.color = Color.black;
+            StartCoroutine(LoadScene(3));
         }
     }
     void Update()
     {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("menu");
+        }
         if (health == 0)
         {
-            Debug.Log("Game Over!");
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            youWinUI.SetActive(true);
+            WinLoseText.text = "Game Over!";
+            WinLoseBG.color = Color.red;
+            WinLoseText.color = Color.white;
+            StartCoroutine(LoadScene(3));
+            // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
     }
-}
+    void SetScoreText()
+    {
+        scoreText.text = "Score: " + score;
+    }
+    void SetHealthText()
+    {
+        healthText.text = "Health: " + health;
+    }
+    IEnumerator LoadScene(float seconds)
+    {
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+} 
